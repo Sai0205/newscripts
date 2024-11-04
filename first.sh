93a7c6e37c33582ce31dc800405ac2dfcc95ae15
deepak.yeddalareddy@gmail.com <deepak.yeddalareddy@gmail.com>
2024-11-04 07:35:22 +0000

    latest

--- first.sh
+++ first.sh
@@ -14,8 +14,17 @@ person3=suneel
 
 echo "$person1 ,$person2 ,$person3 are intellegent "
 
-
+#assigning the linux command soutput to the variable using $()
 DATE=$(date)
 
 echo "todays date is $DATE"
 
+echo
+
+
+#aasining commmand line inputs as variables 
+
+person1=$1
+person2=$2
+
+echo "$person1 and $person2 are friends"

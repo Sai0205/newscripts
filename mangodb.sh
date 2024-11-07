563221ecb57e5c973f925d430728233740e3de44
unknown <deepak>
2024-11-07 16:00:12 +0000

    first

--- mangodb.sh
+++ mangodb.sh
@@ -7,12 +7,12 @@ FILENAME=$0
 LOGFILE=$LOGDIR/${FILENAME}_$DATE.log
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]{
+if [ $USERID -ne 0 ];
     then
         echo "you are not a super user"
         exit 1
 fi
-}
+
 
 VALIDATE(){
     if [ $1 -ne 0 ]
